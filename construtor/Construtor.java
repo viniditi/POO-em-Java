5bf1028238680528165ee9ad4ca46d4ddb01ab1c
Vinícius de Matos Brandão <dtivini@gmail.com>
2022-09-27 22:28:01 -0300

    Construtor

---
+++ construtor/Construtor.java
@@ -0,0 +1,50 @@
+package pooemjava.construtor;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import pooemjava.entidades.Produto2;
+
+public class Construtor {
+
+    
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+                //Por padrão, os atributos são iniciados "vazios".
+        //Produto2 produto2 = new Produto2();  //Agora que temos um contrutor como argumento, nós colocamos a instanciação após lermos os atibutos.
+
+        System.out.println("Insira os dados dos produtos:");
+        System.out.print("Nome: ");
+        String nome = sc.nextLine();
+        System.out.print("Preço: ");
+        double preco = sc.nextDouble();
+        System.out.print("Quantidade em estoque: ");
+        int quantidade = sc.nextInt();
+        
+        Produto2 produto2 = new Produto2(nome,preco,quantidade);
+        
+        System.out.println();
+        System.out.println("Dados do produto: "+produto2.toString());
+        
+        System.out.println();
+        System.out.print("Digite o número de produtos a serem adicionados no estoque: ");
+        quantidade = sc.nextInt();
+        produto2.addProdutos(quantidade);
+        
+        System.out.println();
+        System.out.println("Dados atualizados do produto: "+produto2.toString());
+        
+        System.out.println();
+        System.out.print("Digite o número de produtos a serem removidos: ");
+        quantidade = sc.nextInt();
+        produto2.remocaoDeProdutos(quantidade);
+        
+        System.out.println();
+        System.out.println("Dados atualizados do produto: "+produto2.toString());
+        
+        sc.close();
+    }
+    
+}
---
+++ entidades/Produto2.java
@@ -0,0 +1,38 @@
+package pooemjava.entidades;
+
+
+public class Produto2 {
+    
+    public String nome;
+    public double preco;
+    public int quantidade;
+           //O construtor te obriga a colocar valores nos atributos, e por padrão, ele é colocado abaixo dos atributos.
+    public Produto2(String nome, double preco, int quantidade){
+                    //Eu coloco os atributos no construtor para o parâmetro atribui-los nos atributos.
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+    
+    public double valorTotalEmEstoque(){
+        return preco * quantidade;
+    }
+    
+    public void addProdutos(int quantidade){
+        this.quantidade += quantidade;
+    }
+    
+    public void remocaoDeProdutos(int quantidade){
+        this.quantidade -= quantidade;
+    }
+    public String toString(){
+        return nome
+            + ", $ "
+            +String.format("%.2f", preco)
+            + ", "
+            + quantidade
+            + " unidades, Total: $ "
+            + String.format("%.2f", valorTotalEmEstoque());
+    }
+    
+}
