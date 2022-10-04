1110e561beec73183d2d74c02cdd65159a657ec5
Vinícius de Matos Brandão <dtivini@gmail.com>
2022-10-04 15:50:41 -0300

    Exercícios de Vetores

--- vetores/Vetores.java
+++ vetores/Vetores.java
@@ -57,7 +57,7 @@ public class Vetores {
         System.out.println("Soma: "+soma);
         System.out.println("Média: "+med);*/
         
-        System.out.print("Digite o a quantidade de pessoas: ");
+        /*System.out.print("Digite o a quantidade de pessoas: ");
         int c = sc.nextInt();
 
         
@@ -95,7 +95,82 @@ public class Vetores {
             if(vetor2[i].idade<16){
                 System.out.printf("%s%n",vetor2[i].nome);
             }
+        }*/
+        
+        /*System.out.print("Digite a quantidade de números: ");
+        int d = sc.nextInt();
+        
+        int[] vetor3 = new int[d];
+        
+        int soma1=0;
+        for (int i=0;i<d;i++){
+            System.out.print("Digite um número: ");
+            vetor3[i] = sc.nextInt();
         }
+        
+        System.out.println("\nNúmeros pares:");
+        
+        for (int i=0;i<vetor3.length;i++){
+            
+            if(vetor3[i]%2==0){
+                System.out.print(vetor3[i]+" ");
+                soma1++;
+            }
+        }
+        
+        System.out.println("\nQuantidade de números pares: "+soma1);*/
+        
+        /*System.out.print("Digite o tamanho do vetor: ");
+        int e = sc.nextInt();
+        
+        double[] vetor4 = new double[e];
+        
+        for (int i=0;i<vetor4.length;i++){
+            System.out.print("Digite um número: ");
+            vetor4[i] = sc.nextDouble();
+        }
+        
+        double max = vetor4[0];
+        int posmaior = 0;
+        
+        for(int i=0;i<vetor4.length;i++){
+            if(vetor4[i]>max){
+                max = vetor4[i];
+                posmaior = i;
+            }
+        }
+        
+        System.out.printf("\nMaior valor: %.1f%n",max);
+        System.out.println("Posição do maior valor: "+posmaior);*/
+        
+        System.out.print("Digite o tamanho dos vetores: ");
+        int f = sc.nextInt();
+        double[] vetor5 = new double[f];
+        double[] vetor6 = new double[f];
+        
+        System.out.println("Digite os valores do primeiro vetor:");
+        for (int i=0;i<vetor5.length;i++){
+            vetor5[i] = sc.nextDouble();
+        }
+        
+        System.out.println("Digite os valores do segundo vetor:");
+        for (int i=0;i<vetor6.length;i++){
+            vetor6[i] = sc.nextDouble();
+        }
+        
+        double somaDeTudo=0;
+        System.out.println("\nValor resultante:");
+        for (int i=0;i<f;i++){
+            somaDeTudo = vetor5[i] + vetor6[i];
+            System.out.println(somaDeTudo);
+        }
+        
+        
+        
+        
+        
+        
+        
         
         
         
