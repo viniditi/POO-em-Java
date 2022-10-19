1d3eac55cdaf2d303592eca71f0483ee97a263ff
Vinícius de Matos Brandão <dtivini@gmail.com>
2022-10-18 23:15:21 -0300

    Manipulação de Data e Hora

--- datahora/ManipulacaodeDataeHora.java
+++ datahora/ManipulacaodeDataeHora.java
@@ -18,6 +18,16 @@ public class ManipulacaodeDataeHora {
         d = cal.getTime();
         System.out.println(sdf.format(d));
 
+        /*SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        Date d = Date.from(Instant.parse("2018-06-25T15:42:07Z"));
+        System.out.println(sdf.format(d));
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(d);
+        int minutes = cal.get(Calendar.MINUTE);
+        int month = 1 + cal.get(Calendar.MONTH);
+        System.out.println("Minutes: " + minutes);
+        System.out.println("Month: " + month);*/
+
     }
 
 }
